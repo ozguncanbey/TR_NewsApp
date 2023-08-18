b93c1495133f4920b55c2bf49b8c348c7676fd75
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2023-08-18 14:31:01 +0300

    Initial Commit

---
+++ TR_NewsApp/TR_NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TR_NewsAppApp.swift
+//  TR_NewsApp
+//
+//  Created by Özgün Can Beydili on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TR_NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
